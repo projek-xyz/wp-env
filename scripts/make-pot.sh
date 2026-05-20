3dc147d330ec8eac20563d6a70fdd6ebaccb7aef
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-05-20 08:15:05 +0700

    ci: preseve the `POT-Creation-Date` if there's no translation changes
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/make-pot.sh
+++ scripts/make-pot.sh
@@ -8,23 +8,41 @@ shopt -s nullglob
 make_pot() {
     local pkg_dir="${1%/}"
     local pkg="${pkg_dir##*/}"
+    local pot_file="$pkg_dir/languages/$pkg.pot"
+    local pot_temp=$(mktemp)
 
     if [ ! -d "$pkg_dir" ]; then
         echo -e "\e[1;33mNotice:\e[0m No such directory: $pkg_dir, skipping"
+        rm "$pot_temp"
         return 0
     fi
 
     if [ ! -f "$pkg_dir/.distignore" ]; then
         echo -e "\e[1;33mNotice:\e[0m No .distignore found for $pkg, skipping"
-        continue
+        rm "$pot_temp"
+        return 0
     fi
 
     mkdir -p "$pkg_dir/languages"
 
-    local pot_temp=$(mktemp)
-    local pot_file="$pkg_dir/languages/$pkg.pot"
-
     _wp i18n make-pot "$pkg_dir" "$pot_temp"
+
+    if [ -f "$pot_file" ]; then
+        # Capture ONLY the date value, removing labels, quotes, and the trailing \n
+        local creation_date=$(grep "POT-Creation-Date:" "$pot_file" | head -n 1 | sed 's/.*POT-Creation-Date: \(.*\)\\n.*/\1/')
+
+        if [[ -n "$creation_date" ]]; then
+            local pot_temp_fixed=$(mktemp)
+            # Use | as delimiter and match the exact PO header format
+            sed "s|\"POT-Creation-Date: .*\\\\n\"|\"POT-Creation-Date: $creation_date\\\\n\"|" "$pot_temp" > "$pot_temp_fixed"
+            mv "$pot_temp_fixed" "$pot_temp"
+        fi
+
+        if cmp -s "$pot_temp" "$pot_file"; then
+            rm "$pot_temp"
+            return 0
+        fi
+    fi
 
     mv "$pot_temp" "$pot_file"
 }
