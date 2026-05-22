466d65e75434b4f2d0cb62e0a75bd53d2942dfeb
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-05-23 02:30:56 +0700

    chore: skip if not a wordpress `theme` or `plugin` package
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -39,12 +39,17 @@ make_dist() {
     if [[ ! -f "$pkg_dir/composer.json" || ! -f "$pkg_dir/package.json" ]]; then
         echo -e "\e[1;35mNotice:\e[0m No '\e[1;34mcomposer.json\e[0m' or '\e[1;34mpackage.json\e[0m' found for '\e[1;33m$pkg\e[0m', skipping"
 
-        e_end
         return 0
     fi
 
     pkg_type=$(jq -r '.type' "$pkg_dir/composer.json" | sed 's/wordpress-//')
     pkg_version=$(jq -r '.version' "$pkg_dir/package.json")
+
+    if [[ "$pkg_type" != "plugin" && "$pkg_type" != "theme" ]]; then
+        echo -e "\e[1;35mNotice:\e[0m Unsupported package type '\e[1;33m$pkg_type\e[0m' for '\e[1;33m$pkg\e[0m', skipping"
+
+        return 0
+    fi
 
     if [[ -f "$DIST_DIR/release.json" ]]; then
         manifest_version=$(jq -r ".[\"$pkg\"].version // \"none\"" "$DIST_DIR/release.json")
