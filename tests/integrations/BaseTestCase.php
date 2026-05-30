bea09144284dfc2b19426e49861239eace35dce2
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-05-30 17:36:22 +0700

    chore: override `WP_UnitTestCase_Base::expectDeprecated()`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/integrations/BaseTestCase.php
+++ tests/integrations/BaseTestCase.php
@@ -4,10 +4,35 @@ declare(strict_types=1);
 
 namespace IntegrationTests;
 
+use Override;
+
 /**
  * Base Test Case for integration tests using real WordPress core.
  */
 abstract class BaseTestCase extends \WP_UnitTestCase_Base
 {
-    // .
+    /**
+     * {@inheritdoc}
+     */
+    #[Override]
+    public function expectDeprecated()
+    {
+        // This method is just overriding parent's one
+        // by removing the use of `PHPUnit\Util\Test::parseTestMethodAnnotations`
+        // And keep the rest
+
+        add_action('deprecated_function_run', [$this, 'deprecated_function_run'], 10, 3);
+        add_action('deprecated_argument_run', [$this, 'deprecated_function_run'], 10, 3);
+        add_action('deprecated_class_run', [$this, 'deprecated_function_run'], 10, 3);
+        add_action('deprecated_file_included', [$this, 'deprecated_function_run'], 10, 4);
+        add_action('deprecated_hook_run', [$this, 'deprecated_function_run'], 10, 4);
+        add_action('doing_it_wrong_run', [$this, 'doing_it_wrong_run'], 10, 3);
+
+        add_action('deprecated_function_trigger_error', '__return_false');
+        add_action('deprecated_argument_trigger_error', '__return_false');
+        add_action('deprecated_class_trigger_error', '__return_false');
+        add_action('deprecated_file_trigger_error', '__return_false');
+        add_action('deprecated_hook_trigger_error', '__return_false');
+        add_action('doing_it_wrong_trigger_error', '__return_false');
+    }
 }
