3982e1739b5d4ff8ba4ab529ce1dec5cc1952054
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-06-04 18:23:54 +0700

    bump(custom-theme): release v0.0.5

--- packages/custom-theme/functions.php
+++ packages/custom-theme/functions.php
@@ -9,7 +9,7 @@
 
 namespace Custom_Theme;
 
-defined( 'CUSTOM_THEME_VERSION' ) || define( 'CUSTOM_THEME_VERSION', '0.0.4' );
+defined( 'CUSTOM_THEME_VERSION' ) || define( 'CUSTOM_THEME_VERSION', '0.0.5' );
 
 require_once __DIR__ . '/includes/autoload.php';
 
